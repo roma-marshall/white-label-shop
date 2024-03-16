9d06dcfbfc0fad29abc49d9594a35166ac94b512
roma_marshall <roma_marshall@outlook.com>
2024-03-17 00:40:11 +0100

    add new routers

--- src/router/router.js
+++ src/router/router.js
@@ -2,11 +2,13 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Login from '../components/Login.vue'
 import Register from '../components/Register.vue'
 import Cart from '../components/Cart.vue'
+import Content from '../components/Content.vue'
 
 const routes = [
     { path: '/', component: Login },
     { path: '/register', component: Register },
     { path: '/cart', component: Cart },
+    { path: '/content', component: Content },
 ]
 
 const router = createRouter({
