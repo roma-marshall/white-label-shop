fb6ec7b074ac43ea56352989bd64dca78ce67014
roma_marshall <roma_marshall@outlook.com>
2024-03-18 17:54:35 +0100

    add profile & oder history components

--- src/router/router.js
+++ src/router/router.js
@@ -3,12 +3,16 @@ import Login from '../components/Login.vue'
 import Register from '../components/Register.vue'
 import Cart from '../components/Cart.vue'
 import Content from '../components/Content.vue'
+import Profile from '../components/Profile.vue'
+import History from '../components/History.vue'
 
 const routes = [
     { path: '/', component: Login },
     { path: '/register', component: Register },
     { path: '/cart', component: Cart },
     { path: '/content', component: Content },
+    { path: '/profile', component: Profile },
+    { path: '/history', component: History },
 ]
 
 const router = createRouter({
